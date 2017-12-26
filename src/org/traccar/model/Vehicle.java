6956a879b7715444519a4c89cfc8d89b46946b7d
Srinivas Akula <Srinivas.Akula@e2open.com>
2017-12-26 23:26:16 +0100

    first server commit

---
+++ src/org/traccar/model/Vehicle.java
@@ -0,0 +1,10 @@
+package org.traccar.model;
+
+public class Vehicle extends ExtendedModel {
+
+    private String number;
+    private String model;
+    private int tonnage;
+    private boolean transport;
+
+}
